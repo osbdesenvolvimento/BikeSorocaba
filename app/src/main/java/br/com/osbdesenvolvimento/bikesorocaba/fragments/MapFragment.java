fe3c2c1c8588754f8cfa1f429dc9329e0a84e850
Paulo Gonçalves <pcufscar@gmail.com>
2017-02-12 19:54:14 -0200

    listagem do json

--- app/src/main/java/br/com/osbdesenvolvimento/bikesorocaba/fragments/MapFragment.java
+++ app/src/main/java/br/com/osbdesenvolvimento/bikesorocaba/fragments/MapFragment.java
@@ -2,6 +2,7 @@ package br.com.osbdesenvolvimento.bikesorocaba.fragments;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -28,7 +29,11 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import br.com.osbdesenvolvimento.bikesorocaba.R;
+import br.com.osbdesenvolvimento.bikesorocaba.dtos.Estacao;
+import br.com.osbdesenvolvimento.bikesorocaba.tasks.DownloadJsonAsyncTask;
 
 
 public class MapFragment extends Fragment  {
@@ -36,10 +41,12 @@ public class MapFragment extends Fragment  {
     MapView mMapView;
     private GoogleMap googleMap;
     String lat, lon;
+    MapFragment contextFragmente;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+        contextFragmente = this;
 
         mMapView = (MapView) view.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
@@ -69,52 +76,29 @@ public class MapFragment extends Fragment  {
 
                 // For dropping a marker at a point on the Map
                 LatLng sorocaba = new LatLng(-23.4673332,-47.5176655);
-                googleMap.addMarker(new MarkerOptions().position(sorocaba).title("Marker Title").snippet("Marker Description"));
-
-
-
-                LatLng mark1 = new LatLng(-23.50257,-47.46114);
-                googleMap.addMarker(new MarkerOptions().position(mark1).title("Pra\u00e7a Frei Bara\u00fana").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark2 = new LatLng(-23.50038,-47.4577);
-                googleMap.addMarker(new MarkerOptions().position(mark2).title("Pra\u00e7a Fernando Prestes").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark3 = new LatLng(-23.51575,-47.465);
-                googleMap.addMarker(new MarkerOptions().position(mark3).title("Pq Kasatu Maru").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark4 = new LatLng(-23.50309,-47.46828);
-                googleMap.addMarker(new MarkerOptions().position(mark4).title("P\u00e7a Nove de Julho").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark5 = new LatLng(-23.51493,-47.48941);
-                googleMap.addMarker(new MarkerOptions().position(mark5).title("Armando Pannunzio").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark6 = new LatLng(-23.50078,-47.50812);
-                googleMap.addMarker(new MarkerOptions().position(mark6).title("Ipiranga").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark7 = new LatLng(-23.5019,-47.45276);
-                googleMap.addMarker(new MarkerOptions().position(mark7).title("Ponte da rua XV").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark8 = new LatLng(-23.49181,-47.50672);
-                googleMap.addMarker(new MarkerOptions().position(mark8).title("Wanel Ville").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark9 = new LatLng(-23.48814,-47.47838);
-                googleMap.addMarker(new MarkerOptions().position(mark9).title("Unitem").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark10 = new LatLng(-23.49845,-47.45968);
-                googleMap.addMarker(new MarkerOptions().position(mark10).title("P\u00e7a do Rel\u00f3gio").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark11 = new LatLng(-23.4956,-47.46259);
-                googleMap.addMarker(new MarkerOptions().position(mark11).title("P\u00e7a da Bandeira").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
-
-                LatLng mark12 = new LatLng(-23.48504,-47.46877);
-                googleMap.addMarker(new MarkerOptions().position(mark12).title("P\u00e7a Adolpho Hanickel").snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
+                new DownloadJsonAsyncTask(contextFragmente).execute("https://integrabike.compartibike.com.br/stations.json");
 
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sorocaba).zoom(12).build();
-               googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
         return view;
+    }
+
+    public void atualizaFragment(final List<Estacao> lista){
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                for (Estacao estacao: lista) {
+                    googleMap.addMarker(new MarkerOptions().position(new LatLng(estacao.getGoogleMapX(),estacao.getGoogleMapY())).title(estacao.getName()).snippet("Marker Description").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marker_bike)));
+                }
+
+
+            }
+        });
     }
 
 
