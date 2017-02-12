03b94f61fc640b8e2496ebe0900bc1b8cbf091af
Paulo Gonçalves <pcufscar@gmail.com>
2017-02-12 11:05:59 -0200

    listagem de marcações estasticas para teste

--- app/src/main/java/br/com/osbdesenvolvimento/bikesorocaba/activities/PauloActivity.java
+++ app/src/main/java/br/com/osbdesenvolvimento/bikesorocaba/activities/PauloActivity.java
@@ -1,16 +1,57 @@
 package br.com.osbdesenvolvimento.bikesorocaba.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import br.com.osbdesenvolvimento.bikesorocaba.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PauloActivity extends AppCompatActivity{
+import br.com.osbdesenvolvimento.bikesorocaba.R;
+import br.com.osbdesenvolvimento.bikesorocaba.fragments.MapFragment;
+import br.com.osbdesenvolvimento.bikesorocaba.fragments.Temp1Fragment;
+
+//implements OnMapReadyCallback
+public class PauloActivity extends AppCompatActivity  {
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paulo);
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return new MapFragment();
+                case 1: return new Temp1Fragment();
+                case 2: return new Temp1Fragment();
+                default: return new Temp1Fragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
     }
 
 }
