610553776e60e767fbd35f405189311d5ea172b1
Paulo Gonçalves <pcufscar@gmail.com>
2017-03-11 00:59:30 -0300

    adapter dos fragmentes para a pageview e tabhost

---
+++ app/src/main/java/br/com/osbdesenvolvimento/bikesorocaba/adapters/TabsAdapter.java
@@ -0,0 +1,40 @@
+package br.com.osbdesenvolvimento.bikesorocaba.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import br.com.osbdesenvolvimento.bikesorocaba.fragments.MapFragment;
+import br.com.osbdesenvolvimento.bikesorocaba.fragments.Temp1Fragment;
+
+public class TabsAdapter extends FragmentPagerAdapter {
+
+    public TabsAdapter(FragmentManager fm) {
+
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                MapFragment rFragment1 = new MapFragment();
+                return rFragment1;
+            case 1:
+                Temp1Fragment rFragment2 = new Temp1Fragment();
+                return rFragment2;
+            case 2:
+                Temp1Fragment rFragment3 = new Temp1Fragment();
+                return rFragment3;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+
+        return 3;
+    }
+
+}
